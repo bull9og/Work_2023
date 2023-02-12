b65eb07995c64b99c85ba793d5268d7ef04c7f5b
Mihail Sazonkin <sazonkinmichail@gmail.com>
2023-02-12 15:55:16 +0300

    end site "online coffee shop"

---
+++ Online Coffee Shop/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.section-6__sliderContainer').slick({
+		arrows: true,
+		speed: 1200,
+		infinite: true,
+		draggable: true,
+		dots: false,
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+		centerMode: true,
+	});
+});
